be93db1a9fc03b339ebe30ef9e3c8b6ca8f041db
HimanshuKumar79 <ngukumar123@gmail.com>
2023-10-03 20:27:13 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,7 +24,6 @@ const Card = ({
         <img
           src={url}
           className="object-cover w-full h-full rounded-xl"
-          alt=""
         />
         <div className="absolute left-3 top-3 bg-white px-3 py-2 text-[blue] flex justify-center font-semibold items-center rounded-2xl">
           {forRent ? "For Rent" : "For Sale"}
